0085aa0b7b79a402e9980121a64c1515f6e7e812
Xian <josiexianli@gmail.com>
2018-01-22 15:30:06 -0800

    JOSIE INIT

--- modules/crawler/CYCrawler.js
+++ modules/crawler/CYCrawler.js
@@ -2,26 +2,45 @@ var Crawler = require("js-crawler");
 var filter = require('./CYFilter');
 var connector = require('../mysql_socket/CYConnector');
 
-function startFuck(url) {
+function startCrawl(url) {
+    //console.log('I AM AT START START CRAWL');
     new Crawler().configure({depth: 10})
-        .crawl(url, function(page) {
+        .crawl(url, function (page) {
+            //console.log(page);
             //console.log(page.url);
             //console.log(page.body);
             var list = filter.extractEmails(page.body);
+            var listSMS = filter.extractNumbers(page.body);
+            console.log(page.url);
+            console.log(listSMS);
 
-            if (list !== null){
-                if (list.length > 0){
+            if (list !== null) {
+                if (list.length > 0) {
                     //console.log(list);
                     list.forEach(function (value) {
-                        var query = "INSERT INTO `cy_email` (`email`, `cdate`) VALUES ('"+ value+"' , now() ) ";
+                        var query = "INSERT INTO `cy_email` (`email`, `cdate`) VALUES ('" + value + "' , now() ) ";
+                        console.log(query);
                         connector.performQuery(query, function (err, result) {
                             //console.log(err);
+
+                        });
+                    })
+                }
+            }
+            if (listSMS != null) {
+                if (listSMS.length > 0) {
+                    listSMS.forEach(function (value) {
+                        var query = "INSERT INTO `jl_sms` (`cell`, `cdate`) VALUES ('" + value + "' , now() ) ";
+                        console.log(query);
+                        connector.performQuery(query, function (err, result) {
+                            //console.log(err);
+
                         });
                     })
                 }
             }
             //console.log(page);
-        }, function(response) {
+        }, function (response) {
             // console.log("ERROR occurred:");
             // console.log(response.status);
             // console.log(response.url);
@@ -29,5 +48,19 @@ function startFuck(url) {
         });
 }
 
+function testCrawl(text) {
+    var list = filter.extractEmails(text);
+    if (list !== null) {
+        list.forEach(function (value) {
+            console.log(value);
+        });
+    }else{
+        console.log('CANNOT FIND ANY MATCH IN ' + "'" + text + "'");
 
-module.exports.startFuck = startFuck;
+    }
+
+}
+
+
+module.exports.startCrawl = startCrawl;
+module.exports.testCrawl = testCrawl;
--- modules/crawler/CYFilter.js
+++ modules/crawler/CYFilter.js
@@ -1,9 +1,32 @@
 function extractEmails (text)
-{
-    var emails = [];
-    //var temps = text.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
-    var temps = text.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.com+)/gi);
+ {
+   var emails = [];
+  // var temps = text.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
+   var temps = text.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.com+)/gi);
 
+     if (temps !== null){
+          temps.forEach(function (value) {
+            if (value.indexOf('png') <0 && value.indexOf('jpg') < 0 && value.indexOf('-') < 0){
+               emails.push(value);
+             }
+          });
+         temps.forEach(function (value) {
+             emails.push(value);
+         });
+     }else{
+         return null;
+   }
+
+     return emails;
+ }
+
+function extractNumbers(text)
+{
+
+    console.log(text);
+    var numbers = [];
+    //var temps = text.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
+    var temps = text.match(/^1[3|4|5|7|8][0-9]{9}$/g);
     if (temps !== null){
         // temps.forEach(function (value) {
         //     if (value.indexOf('png') <0 && value.indexOf('jpg') < 0 && value.indexOf('-') < 0){
@@ -11,18 +34,16 @@ function extractEmails (text)
         //     }
         // });
         temps.forEach(function (value) {
-            emails.push(value);
+            numbers.push(value);
         });
+        //console.log('We got a match!!!');
     }else{
         return null;
     }
+    return numbers;
 
-
-
-
-    return emails;
 }
 
 
-
+module.exports.extractNumbers = extractNumbers;
 module.exports.extractEmails = extractEmails;
